9e74a61e1dd7620734e47af2a90df7ab0a461d9c
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2024-01-24 18:05:15 +0100

    changes in login-component

--- MYSProject/src/Login/Login.jsx
+++ MYSProject/src/Login/Login.jsx
@@ -1,3 +1,4 @@
+import classes from "./Login.module.scss"
 import { useState } from "react"
 const Login=()=>{
 const[email,setEmail]=useState("")
@@ -9,14 +10,17 @@ const handleSubmit=(e)=>{
     
 }
 return(
-    <div className="container-login">
-        <div className="login">
-            <form onSubmit={handleSubmit}>
+    <div className={classes.containerlogin}>
+        <div className={classes.login}>
+            <form className={classes.form}onSubmit={handleSubmit}>
             <input type="text"name="email"value={email}placeholder="Email" onChange={((e)=>setEmail(e.target.value))} />
             <input type="password"name="password"value={password}placeholder="Password"onChange={((e)=>setPassword(e.target.value))} />
             <button type="submit">Log in</button>
         </form>
         <button>Don't have an account? Register</button>
+        </div>
+        <div className={classes.imgform}>
+            <img src="" alt="" />
         </div>
         
     </div>
