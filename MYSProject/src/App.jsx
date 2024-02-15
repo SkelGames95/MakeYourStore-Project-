cbe24fcff76e5ebf1c2ec3d69b5dbd8c799ee443
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-02-15 17:36:10 +0100

    logreg

--- MYSProject/src/App.jsx
+++ MYSProject/src/App.jsx
@@ -1,11 +1,9 @@
-import Login from "./Login/Login";
 import { Homepage } from "./Pages/Homepage";
 
 export const App = () => {
   return (
     <div>
-      {/* <Homepage /> */}
-      <Login />
+      <Homepage />
     </div>
   );
 };
