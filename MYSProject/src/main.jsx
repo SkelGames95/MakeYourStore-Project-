768b3a19eb878f962626ab5c50598864d60f677b
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-02-15 17:33:00 +0100

    rendering

--- MYSProject/src/main.jsx
+++ MYSProject/src/main.jsx
@@ -1,7 +1,7 @@
 import { React } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter}from "react-router-dom"
-import { App } from './App.jsx'
+import { App } from './LogApp'
 import "./Login/Login.module.scss"
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
