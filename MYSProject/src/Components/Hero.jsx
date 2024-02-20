9cc2f692457453b95066931f747c198a1aaa4e96
Elio <elio.sanfratello@gmail.com>
2024-02-20 12:15:16 +0100

    sistemata homepage

--- MYSProject/src/Components/Hero.jsx
+++ MYSProject/src/Components/Hero.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 import { HomepageButton } from "./HomepageButton";
 import { Link } from "react-router-dom";
+import Button from "../Button-LogReg/Button";
 export function Hero() {
   return (
     <>
@@ -10,19 +11,22 @@ export function Hero() {
         <div className="img-responsive-wrapper">
           <div className="img-responsive">
             <div className="img-wrapper">
-              <img
+              {/* <img
                 className="hero-img"
                 src="../assets/images/background-hero.jpg"
                 title="titolo immagine"
                 alt="descrizione immagine"
-              />
+              /> */}
             </div>
           </div>
         </div>
 
-        <Link to="/shop">
-          <HomepageButton className="button" content={"SHOP NOW"} />
-        </Link>
+        <div className="positionButton">
+          <Link to="/shop">
+            {/* <HomepageButton className="button" content={"SHOP NOW"} /> */}
+            <Button label="SHOP NOW" className="buttonShopNow" />
+          </Link>
+        </div>
       </section>
     </>
   );
