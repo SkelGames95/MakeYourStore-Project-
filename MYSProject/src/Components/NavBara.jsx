16a6880db0841654c2ea1d0f88552225a7afd862
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-02-20 18:21:15 +0100

    navbar changes

--- MYSProject/src/Components/NavBara.jsx
+++ MYSProject/src/Components/NavBara.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import "./NavBara.css" 
+import React from "react";
+import "./NavBara.css";
 import MYSTitle from "../../images/MYSTitleSmall.png";
+import { Link } from "react-router-dom";
 const NavBara = () => {
   return (
     <header className="header">
-      <a href="/" className="logo"><img src={MYSTitle} /></a>
+      <Link to="/" className="logo">
+        <img src={MYSTitle} />
+      </Link>
       <input className="menu-btn" id="menu-btn" type="checkbox" />
-      <label htmlFor="menu-btn" className="menu-icon"><span className="nav-icon"></span></label>
+      <label htmlFor="menu-btn" className="menu-icon">
+        <span className="nav-icon"></span>
+      </label>
       <ul className="menu">
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#shop">Shop</a></li>
-        <li><a href="#contacts">Contacts</a></li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#shop">Shop</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+        <li>
+          <Link>Login</Link>
+        </li>
       </ul>
     </header>
   );
