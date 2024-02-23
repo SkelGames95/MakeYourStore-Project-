e75090df059f1bbf213c725d8d74ab57343a3cd7
Elio <elio.sanfratello@gmail.com>
2024-02-23 09:37:43 +0100

    implementata navigazione nel carrello

--- MYSProject/src/pages/Carrello.jsx
+++ MYSProject/src/pages/Carrello.jsx
@@ -33,18 +33,20 @@ export function Carrello() {
             {cart && cart.map((product, index) => (
                 <div className="product-container" key={index}>
                     <div className="imageDetails">
-                        <img src={product.image} alt=""/>
+                        <Link to={`/shop/${product.id}`}>
+                            <img src={product.image} alt="" />
+                        </Link>
                         <div className="productDetails">
                             <p><span>Product</span>: {product.name}</p>
                             <p><span>Price</span>: {product.price}€</p>
                             <p><span>Quantity </span>: {product.quantity}</p>
                         </div>
-                        <Button className="remove" label="Remove" onClick={() => handleRemove(index)}/>
+                        <Button className="remove" label="Remove" onClick={() => handleRemove(index)} />
                         <div className="separating"></div>
                     </div>
                 </div>
             ))}
-            {cart && cart.length === 0 && <p className='alert2'>Oops, your cart is empty!</p> }
+            {cart && cart.length === 0 && <p className='alert2'>Oops, your cart is empty!</p>}
             {cart && cart.length > 0 && <div className="cart-total">
                 {cartTotal && <p><span>Total</span>: {cartTotal.toFixed(2)}€</p>}
                 <Link to="https://www.paypal.com/signin?intent=developer&returnUri=https%3A%2F%2Fdeveloper.paypal.com%2Fdeveloper%2Fapplications"><Button label="Checkout" className="checkout" /></Link>
