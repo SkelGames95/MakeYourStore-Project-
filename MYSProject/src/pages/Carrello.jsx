3dcf85230f743d1ecd3aa69e92ef949d3f9e761b
Ruben <rubencirelli4@gmail.com>
2024-02-29 19:08:50 +0100

    Checkout completo

--- MYSProject/src/pages/Carrello.jsx
+++ MYSProject/src/pages/Carrello.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import "./Carrello.css"
 import { Button } from '../Components/Button';
+import { loadStripe } from "@stripe/stripe-js";
+import { useNavigate} from "react-router-dom"
 
 
 export function Carrello() {
 
     const [cart, setCart] = useState([])
+    const navigate = useNavigate();
 
     useEffect(() => {
         const product = localStorage.getItem("carrello");
@@ -26,6 +29,43 @@ export function Carrello() {
     const cartTotal = cart ? cart.reduce((total, product) => {
         return total + (Number(product.price) * Number(product.quantity));
     }, 0) : 0;
+
+    const makePayment = async () => {
+        try {
+          // Create a Stripe client
+          const stripe = await loadStripe(
+            "pk_test_51Ok6pQFebw0p02EeDOKgRhiONS1KIObtnaghoamB0niM0qfbTNALDWQH2ekDG9we72rI43E45bnf4HOos7039SJF00zcYFLrJu"
+          );
+          const body = {
+            products: cart,
+          };
+          const headers = {
+            "Content-type": "application/json",
+          };
+          const response = await fetch(
+            "http://localhost:3000/create-checkout-session",
+            {
+              method: "POST",
+              headers: headers,
+              mode: "cors",
+              body: JSON.stringify(body),
+            }
+          );
+          if (!response.ok) {
+            console.log("Failed to create checkout session:", response.status);
+            return;
+          }
+          
+          const session = await response.json();
+        
+          navigate(stripe.redirectToCheckout({
+            sessionId:session.id
+          }))
+          
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
 
     return (
         <div className="carrello-container">
@@ -49,7 +89,7 @@ export function Carrello() {
             {cart && cart.length === 0 && <p className='alert2'>Oops, your cart is empty!</p>}
             {cart && cart.length > 0 && <div className="cart-total">
                 {cartTotal && <p><span>Total</span>: {cartTotal.toFixed(2)}€</p>}
-                <Link to="https://www.paypal.com/signin?intent=developer&returnUri=https%3A%2F%2Fdeveloper.paypal.com%2Fdeveloper%2Fapplications"><Button label="Checkout" className="checkout" /></Link>
+                <Button label="Checkout" className="checkout" onClick={makePayment }/>
             </div>}
         </div>
     );
